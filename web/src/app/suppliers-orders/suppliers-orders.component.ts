06683aff57db2f0585f38e805131192cc62134fc
edramos-97 <up201505779@fe.up.pt>
2018-11-25 15:07:39 +0000

    fix date displaying incorrect days delta

--- web/src/app/suppliers-orders/suppliers-orders.component.ts
+++ web/src/app/suppliers-orders/suppliers-orders.component.ts
@@ -2,7 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { Order } from '@app/_models';
 
 const ORDER_DATA: Order[] = [
-  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 26), content: [
+  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 24), content: [
+    {
+      name: 'example product',
+      reference: 123456789,
+      quantity: 10,
+      location: 'A1.1.10.14',
+      warehouse: 'A1',
+    },
+    {
+      name: 'example product',
+      reference: 123456789,
+      quantity: 10,
+      location: 'A1.1.10.14',
+      warehouse: 'A1',
+    },
+    {
+      name: 'example product',
+      reference: 123456789,
+      quantity: 10,
+      location: 'A1.1.10.14',
+      warehouse: 'A1',
+    },
+    {
+      name: 'example product',
+      reference: 123456789,
+      quantity: 10,
+      location: 'A1.1.10.14',
+      warehouse: 'A1',
+    }
+  ]},
+  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 25), content: [
     {
       name: 'example product',
       reference: 123456789,
@@ -62,7 +92,7 @@ const ORDER_DATA: Order[] = [
       warehouse: 'A1',
     }
   ]},
-  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 26), content: [
+  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 20), content: [
     {
       name: 'example product',
       reference: 123456789,
@@ -92,7 +122,7 @@ const ORDER_DATA: Order[] = [
       warehouse: 'A1',
     }
   ]},
-  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 26), content: [
+  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 30), content: [
     {
       name: 'example product',
       reference: 123456789,
@@ -122,7 +152,7 @@ const ORDER_DATA: Order[] = [
       warehouse: 'A1',
     }
   ]},
-  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 26), content: [
+  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 20), content: [
     {
       name: 'example product',
       reference: 123456789,
@@ -152,7 +182,7 @@ const ORDER_DATA: Order[] = [
       warehouse: 'A1',
     }
   ]},
-  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 26), content: [
+  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 20), content: [
     {
       name: 'example product',
       reference: 123456789,
@@ -182,7 +212,7 @@ const ORDER_DATA: Order[] = [
       warehouse: 'A1',
     }
   ]},
-  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 26), content: [
+  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 20), content: [
     {
       name: 'example product',
       reference: 123456789,
@@ -212,7 +242,7 @@ const ORDER_DATA: Order[] = [
       warehouse: 'A1',
     }
   ]},
-  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 26), content: [
+  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 20), content: [
     {
       name: 'example product',
       reference: 123456789,
@@ -242,37 +272,7 @@ const ORDER_DATA: Order[] = [
       warehouse: 'A1',
     }
   ]},
-  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 26), content: [
-    {
-      name: 'example product',
-      reference: 123456789,
-      quantity: 10,
-      location: 'A1.1.10.14',
-      warehouse: 'A1',
-    },
-    {
-      name: 'example product',
-      reference: 123456789,
-      quantity: 10,
-      location: 'A1.1.10.14',
-      warehouse: 'A1',
-    },
-    {
-      name: 'example product',
-      reference: 123456789,
-      quantity: 10,
-      location: 'A1.1.10.14',
-      warehouse: 'A1',
-    },
-    {
-      name: 'example product',
-      reference: 123456789,
-      quantity: 10,
-      location: 'A1.1.10.14',
-      warehouse: 'A1',
-    }
-  ]},
-  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 26), content: [
+  { supplier: 'SOFRIO', name: 'ECL', date: new Date(2018, 10, 20), content: [
     {
       name: 'example product',
       reference: 123456789,
@@ -316,8 +316,17 @@ export class SuppliersOrdersComponent implements OnInit {
 
   constructor() { }
 
-  dateDiff(date1: any, date2: any): number {
-    return Math.floor((date1 - date2) / 1000 / 60 / 60 / 24);
+  dateDiff(date1: any, date2: any): string {
+    const val = Math.ceil((date1 - date2) / 1000 / 60 / 60 / 24);
+   if (val === 1) {
+     return Math.abs(val) + ' day left';
+   } else if (val === -1) {
+     return Math.abs(val) + ' day overdue';
+   } else if (val >= 0) {
+     return Math.abs(val) + ' days left';
+   } else {
+     return Math.abs(val) + ' days overdue';
+   }
   }
 
   ngOnInit() {
